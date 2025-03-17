9f13e659376b220f978f8279f36db93e5fa9de75
José Duarte <duarte.gmj@gmail.com>
2025-03-17 10:18:36 +0000

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,9 @@
+{
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        buildInputs = with pkgs; [ nodePackages.pnpm biome ];
+      in with pkgs; { devShells.default = mkShell { inherit buildInputs; }; });
+}
