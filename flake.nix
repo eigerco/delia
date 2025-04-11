e38579dd5634817340789d77a6124d2e7b5e16ef
aidan46 <aidansullivan.dev@gmail.com>
2025-04-11 17:36:18 +0200

    fix: Proper Rust in nix

--- flake.nix
+++ flake.nix
@@ -1,9 +1,27 @@
 {
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = { nixpkgs.follows = "nixpkgs"; };
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        buildInputs = with pkgs; [ git nodejs nodePackages.pnpm biome rustc cargo ];
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile
+          ./rust-toolchain.toml;
+
+        buildInputs = with pkgs; [
+          biome
+          git
+          nodejs
+          nodePackages.pnpm
+          rustToolchain
+          wasm-pack
+        ];
       in with pkgs; { devShells.default = mkShell { inherit buildInputs; }; });
 }
