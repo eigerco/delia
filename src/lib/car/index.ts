ceb33734328067d7846f252a658aa2cb1f2ddcbc
aidan46 <aidansullivan.dev@gmail.com>
2025-04-15 16:07:18 +0200

    fix: Update multihash width index header

--- src/lib/car/index.ts
+++ src/lib/car/index.ts
@@ -186,7 +186,7 @@ export function indexHeader(entriesCount: number): Uint8Array {
     encode(MULTIHASH_INDEX_SORTED_CODE), // multicodec varint
     numberToU32LE(0x01), // digest bucket length
     numberToU64LE(SHA_256_CODE), // multihash code
-    numberToU32LE(entriesCount), // entries amount
+    numberToU32LE(0x01), // unique widths under the same multihash code (always one for SHA256)
     numberToU32LE(entrySize), // digest + offset length
     numberToU64LE(entrySize * entriesCount), // total length ((digest + offset) * entries)
   );
