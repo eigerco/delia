82c43ade567abdc80fb8ca41dbac502ccc52dd83
aidan46 <aidansullivan.dev@gmail.com>
2025-05-14 11:52:46 +0200

    fix: Remove /delia path

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/delia/", // GitHub Pages repository name
+  // base: "/delia/", // GitHub Pages repository name
 });
