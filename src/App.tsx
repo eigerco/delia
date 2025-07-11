6586c3e02dfb90b662b771ce549a5eb6322b2b72
José Duarte <duarte.gmj@gmail.com>
2025-07-11 15:52:10 +0100

    feat: refresh page on address "unfocus" and Enter press

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import type { InjectedAccountWithMeta } from "@polkadot/extension-inject/types";
-import { AlertCircle, Loader2, RefreshCw } from "lucide-react";
+import { AlertCircle, Loader2 } from "lucide-react";
 import { useEffect, useMemo, useState } from "react";
 import { Link, Outlet, useLocation } from "react-router";
 import { default as initWasm } from "wasm-commp";
@@ -7,46 +7,9 @@ import { useCtx } from "./GlobalCtx";
 import { GlobalCtxProvider } from "./GlobalCtxProvider";
 import { ConnectWallet } from "./components/ConnectWallet";
 import Toaster from "./components/Toaster";
+import { WsAddressInput } from "./components/WsAddressInput";
 import { DOWNLOAD_PATH, INDEX_PATH } from "./lib/consts";
 import { setupTypeRegistry } from "./lib/registry";
-
-function WsAddressInput() {
-  const { wsAddress, setWsAddress } = useCtx();
-
-  // We use a local state copy so users can type freely
-  // without triggering a reconnect on every keystroke.
-  const [localValue, setLocalValue] = useState(wsAddress);
-
-  // Keep localValue in sync with context if it changes externally
-  useEffect(() => {
-    setLocalValue(wsAddress);
-  }, [wsAddress]);
-
-  return (
-    <>
-      <label htmlFor="ws-address" className="text-gray-700 mr-2">
-        Address
-      </label>
-      <input
-        id="ws-address"
-        type="text"
-        className="p-1 border rounded focus:ring-blue-500 focus:border-blue-500 mr-2"
-        value={localValue}
-        onChange={(e) => setLocalValue(e.target.value)}
-      />
-      <button
-        type="submit"
-        className="transition-colors hover:text-blue-600"
-        onClick={() => {
-          setWsAddress(localValue); // Triggers reconnection
-        }}
-        title="Connect"
-      >
-        <RefreshCw />
-      </button>
-    </>
-  );
-}
 
 function App() {
   const [accounts, setAccounts] = useState<InjectedAccountWithMeta[]>([]);
--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -4,7 +4,6 @@ import type { TokenProperties } from "../GlobalCtx";
 
 // NOTE: ideally, this module will be gone once we get the ports issue settled
 export const DEFAULT_LOCAL_STORAGE_ADDRESS = { ip: "127.0.0.1", port: 8001 };
-export const DEFAULT_LOCAL_RPC_ADDRESS = { ip: "127.0.0.1", port: 8000 };
 export const COLLATOR_LOCAL_RPC_URL = "ws://127.0.0.1:42069"; // TODO: replace with some mechanism like polkadot.js
 // Block Time in milliseconds, needs to match current runtime.
 export const BLOCK_TIME = 6000;
